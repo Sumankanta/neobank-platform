04cb60ff703e4a6644dfc4893a325acf1b6b5279
sumankanta <sumankpadhan@gmail.com>
2026-06-27 20:00:54 +0530

    fix(cors): switch API calls to HTTPS to resolve preflight redirect error

--- NeoBank-Backend/src/main/java/com/infy/NeoBank/config/SecurityConfig.java
+++ NeoBank-Backend/src/main/java/com/infy/NeoBank/config/SecurityConfig.java
@@ -79,8 +79,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
-//        config.setAllowedOrigins(List.of("http://http://localhost:58301"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "http://localhost:4300",
+                "https://neobank-platform-production.up.railway.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
